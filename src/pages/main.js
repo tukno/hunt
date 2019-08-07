a39dc60f20302aa044c299890ddecb989ca112d2
tukno <oliveiradavid007@gmail.com>
2019-08-07 02:48:17 -0400

    Listando produtos

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import api from '../services/api';
 
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 
 export default class Main extends Component {
-
     static navigationOptions = {
         title: 'JSHunt',
         headerStyle:{
@@ -12,6 +11,10 @@ export default class Main extends Component {
         },
         headerTintColor: "#FFF"
     }
+
+    state = {
+        docs: []
+    };
 
     componentDidMount(){
         this.loadProducts();
@@ -22,13 +25,29 @@ export default class Main extends Component {
         const response = await api.get('/products');
         const { docs } = response.data;
 
-        console.log(docs);
+        this.setState({ docs });
     }
+
+    //Render a product
+    renderItem = ({ item }) => (
+        <View>
+            <Text>{ item.title }</Text>
+            <Text>{ item.description }</Text>
+
+            <TouchableOpacity onPress={ () => {} }>
+                <Text>Acessar</Text>
+            </TouchableOpacity>
+        </View>
+    );
 
     render(){
         return(
             <View>
-                <Text>Página Main</Text>
+                <FlatList
+                    data={ this.state.docs }
+                    keyExtractor={item => item._id}
+                    renderItem={ this.renderItem }
+                />
             </View>
         );
     }
